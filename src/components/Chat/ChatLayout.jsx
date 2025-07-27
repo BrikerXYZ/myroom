8ceba2f9769c561ef571419ee5379dd1a8027384
Andri Ridwan Pratama <278535+andri3d@users.noreply.github.com>
2025-07-27 18:39:12 +0700

    Update ChatLayout.jsx

--- src/components/Chat/ChatLayout.jsx
+++ src/components/Chat/ChatLayout.jsx
@@ -4,7 +4,7 @@ const ChatLayout = ({ children }) => {
   return (
     <div className='flex flex-col w-80 bg-black rounded-lg chat-shadow relative'>
       <div className='font-bold text-lg my-1 py-1 border-b border-gray-100'>
-        Messages
+        Group Chat
       </div>
       {children}
     </div>
