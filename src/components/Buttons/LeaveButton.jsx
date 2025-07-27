dbfb11410daae0feea747914be19006d6f180d37
Andri Ridwan Pratama <278535+andri3d@users.noreply.github.com>
2025-07-27 18:40:03 +0700

    Update LeaveButton.jsx

--- src/components/Buttons/LeaveButton.jsx
+++ src/components/Buttons/LeaveButton.jsx
@@ -6,7 +6,7 @@ const LeaveButton = ({ onClick }) => {
       className='px-6 py-3 w-40 bg-red-600 text-white font-bold hover:opacity-80 rounded-3xl border-none focus:ring-1 ring-brand-100 outline-none'
       onClick={onClick}
     >
-      Leave
+      Akhiri
     </button>
   );
 };
